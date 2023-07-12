f4af6dbbda763d0cf52f2b93ae988ec25141536f
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-07-12 18:29:43 +0300

    change ports

---
+++ src/test/java/ru/dmitriypetrov/newclient/NewClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitriypetrov.newclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
